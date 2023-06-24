891d54288afc090233bb0d5deaaad3b537899c6c
heinrichbui <50565877+heinrichbui@users.noreply.github.com>
2023-06-24 21:39:26 +0200

    Add files via upload

---
+++ newAvdHost.ps1
@@ -0,0 +1,110 @@
+##############################
+#    AVD Script Parameters   #
+##############################
+Param (        
+    [Parameter(Mandatory=$true)]
+        [string]$RegistrationToken          
+)
+
+
+######################
+#    AVD Variables   #
+######################
+$LocalAVDpath            = "c:\temp\AVD\"
+$AVDBootURI              = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrxrH'
+$AVDAgentURI             = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv'
+$AVDAgentInstaller       = 'AVD-Agent.msi'
+$AVDBootInstaller        = 'AVD-Bootloader.msi'
+
+
+####################################
+#    Test/Create Temp Directory    #
+####################################
+if((Test-Path c:\temp) -eq $false) {
+    Add-Content -LiteralPath C:\New-AVDSessionHost.log "Create C:\temp Directory"
+    Write-Host `
+        -ForegroundColor Cyan `
+        -BackgroundColor Black `
+        "creating temp directory"
+    New-Item -Path c:\temp -ItemType Directory
+}
+else {
+    Add-Content -LiteralPath C:\New-AVDSessionHost.log "C:\temp Already Exists"
+    Write-Host `
+        -ForegroundColor Yellow `
+        -BackgroundColor Black `
+        "temp directory already exists"
+}
+if((Test-Path $LocalAVDpath) -eq $false) {
+    Add-Content -LiteralPath C:\New-AVDSessionHost.log "Create C:\temp\AVD Directory"
+    Write-Host `
+        -ForegroundColor Cyan `
+        -BackgroundColor Black `
+        "creating c:\temp\AVD directory"
+    New-Item -Path $LocalAVDpath -ItemType Directory
+}
+else {
+    Add-Content -LiteralPath C:\New-AVDSessionHost.log "C:\temp\AVD Already Exists"
+    Write-Host `
+        -ForegroundColor Yellow `
+        -BackgroundColor Black `
+        "c:\temp\AVD directory already exists"
+}
+New-Item -Path c:\ -Name New-AVDSessionHost.log -ItemType File
+Add-Content `
+-LiteralPath C:\New-AVDSessionHost.log `
+"
+RegistrationToken = $RegistrationToken
+Optimize          = $Optimize
+"
+
+
+#################################
+#    Download AVD Components    #
+#################################
+Add-Content -LiteralPath C:\New-AVDSessionHost.log "Downloading AVD Boot Loader"
+    Invoke-WebRequest -Uri $AVDBootURI -OutFile "$LocalAVDpath$AVDBootInstaller"
+Add-Content -LiteralPath C:\New-AVDSessionHost.log "Downloading AVD Agent"
+    Invoke-WebRequest -Uri $AVDAgentURI -OutFile "$LocalAVDpath$AVDAgentInstaller"
+
+
+################################
+#    Install AVD Components    #
+################################
+Add-Content -LiteralPath C:\New-AVDSessionHost.log "Installing AVD Bootloader"
+$bootloader_deploy_status = Start-Process `
+    -FilePath "msiexec.exe" `
+    -ArgumentList "/i $AVDBootInstaller", `
+        "/quiet", `
+        "/qn", `
+        "/norestart", `
+        "/passive", `
+        "/l* $LocalAVDpath\AgentBootLoaderInstall.txt" `
+    -Wait `
+    -Passthru
+$sts = $bootloader_deploy_status.ExitCode
+Add-Content -LiteralPath C:\New-AVDSessionHost.log "Installing AVD Bootloader Complete"
+Write-Output "Installing RDAgentBootLoader on VM Complete. Exit code=$sts`n"
+Wait-Event -Timeout 5
+Add-Content -LiteralPath C:\New-AVDSessionHost.log "Installing AVD Agent"
+Write-Output "Installing RD Infra Agent on VM $AgentInstaller`n"
+$agent_deploy_status = Start-Process `
+    -FilePath "msiexec.exe" `
+    -ArgumentList "/i $AVDAgentInstaller", `
+        "/quiet", `
+        "/qn", `
+        "/norestart", `
+        "/passive", `
+        "REGISTRATIONTOKEN=$RegistrationToken", "/l* $LocalAVDpath\AgentInstall.txt" `
+    -Wait `
+    -Passthru
+Add-Content -LiteralPath C:\New-AVDSessionHost.log "AVD Agent Install Complete"
+Wait-Event -Timeout 5
+
+
+
+##########################
+#    Restart Computer    #
+##########################
+Add-Content -LiteralPath C:\New-AVDSessionHost.log "Process Complete - REBOOT"
+Restart-Computer -Force
